7214fd413824a4bd7125844cd0009afa84ebb0d8
jhondiegoasd-source <jhon.diegoasd@gmail.com>
2026-02-09 01:12:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function abrirCarta(){
-document.getElementById("carta").style.display="block";
+document.getElementById("sobre").classList.add("abierto");
 }
 
-function playMusic(){
-document.getElementById("musica").play();
-}
-
-/* Contador */
-const inicio = new Date("2024-02-14");
+/* contador */
+const inicio = new Date("2024-02-09");
 
 function actualizar(){
 const hoy = new Date();
@@ -21,7 +17,7 @@ document.getElementById("contador").innerText =
 setInterval(actualizar,1000);
 actualizar();
 
-/* Corazones cayendo */
+/* corazones */
 function crearCorazon(){
 const heart = document.createElement("div");
 heart.classList.add("corazon");
@@ -30,6 +26,10 @@ heart.style.left=Math.random()*100+"vw";
 heart.style.animationDuration=3+Math.random()*3+"s";
 
 document.body.appendChild(heart);
+setTimeout(()=>heart.remove(),6000);
+}
+
+setInterval(crearCorazon,300);
 
 setTimeout(()=>heart.remove(),6000);
 }
