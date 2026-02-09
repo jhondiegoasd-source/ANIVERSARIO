9e366d36d757a6998ae30937eca4ecd9ff33c135
jhondiegoasd-source <jhon.diegoasd@gmail.com>
2026-02-09 01:16:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-function abrirCarta(){
+function iniciarPagina(){
+document.getElementById("inicio").style.display="none";
+
+document.querySelector(".contenido").classList.add("visible");
+
+document.getElementById("musica").play();
+
+/* fotos aparecen */
+setTimeout(()=>{
+document.querySelector(".galeria").classList.add("visible");
+},1000);
+
+/* carta se abre */
+setTimeout(()=>{
 document.getElementById("sobre").classList.add("abierto");
+},2500);
+
+/* mensaje final */
+setTimeout(()=>{
+document.getElementById("mensajeFinal").classList.add("visible");
+},5000);
 }
 
 /* contador */
-const inicio = new Date("2024-02-09");
+const inicioFecha = new Date("2024-02-09");
 
 function actualizar(){
 const hoy = new Date();
-const diff = hoy - inicio;
+const diff = hoy - inicioFecha;
 const dias = Math.floor(diff/(1000*60*60*24));
 
 document.getElementById("contador").innerText =
@@ -30,6 +49,7 @@ setTimeout(()=>heart.remove(),6000);
 }
 
 setInterval(crearCorazon,300);
+
 
 setTimeout(()=>heart.remove(),6000);
 }
